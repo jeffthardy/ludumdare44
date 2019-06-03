4f053fe36bbb25b16e87600fd1cedb859ae6c86c
Jeff Hardy <jeffthardy@gmail.com>
2019-06-03 18:48:38 -0500

    Adding theme music and villain audio

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private static MusicManager instance = null;
+    public static MusicManager Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (instance != null && (instance != this))
+        {
+            // Destroy the master singleton if going to a scene we don't use the music player
+            if (((scene.name != "Menu") && (scene.name != "About")))
+                Destroy(instance.gameObject);
+
+            Destroy(this.gameObject);
+
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // any other methods you need
+}
+
--- Assets/scripts/IntroController.cs
+++ Assets/scripts/IntroController.cs
@@ -5,18 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class IntroController : MonoBehaviour
 {
-    public float fixedIntroTime = 3.0f;
+    public float fixedIntroTime = 2.0f;
+    public GameObject villain;
+
+    private AudioSource villainAudio;
+    private bool introStarted;
 
     // Start is called before the first frame update
     void Start()
     {
+        introStarted = false;
+        villainAudio = villain.GetComponent<AudioSource>();
         StartCoroutine(IntroPlayer());
+
+    }
+
+    private void Update()
+    {
+        // Finished audio intro
+        if((villainAudio.isPlaying == false) && (introStarted == true))
+        {
+            //Speech is over... start level
+            SceneManager.LoadScene("Level1");
+        }
 
     }
 
     IEnumerator IntroPlayer()
     {
+        // Wait for camera to fade in
         yield return new WaitForSeconds(fixedIntroTime);
-        SceneManager.LoadScene("Level1");
+
+        //Listen to villan's speech
+        villainAudio.Play();
+        introStarted = true;
     }
 }
